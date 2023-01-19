5d5fcb4b990eee7a9a15cf67a20563c6c87d1d4b
Montukhan786 <montukhanpathan@gmail.com>
2023-01-19 12:00:05 +0530

    solution 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,20 @@ package com.driver;
 import java.util.Scanner;
 
 public class Main {
+
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y,int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
     public static void main(String[] args) {
 
         Product p = new Product();
@@ -14,16 +28,3 @@ public class Main {
     }
 }
 
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-    public int product(int x, int y,int z) {
-        return x*y*z;
-    }
-
-    public double product(double x, double y) {
-        return x*y;
-    }
-
-}
